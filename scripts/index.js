997376ca52c4042329aee7868da70dd088bf4259
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-13 12:17:15 +0200

    hm_7

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
 'use strict';
+const a = +prompt ('Enter the first number, please');
+const b = +prompt('Enter the second number, please');
+const c = prompt('Enter the number to check, please');
+
+function firstExample(a,b) {
+return a === b;
+}
+console.log(firstExample(a,b));
+
+function secondExample(a,b) {
+   return (a+b) > 10;
+}
+console.log(secondExample(a,b));
+
+function thirdExample(c) {
+   return c < 0;
+}
+console.log(thirdExample(c));
