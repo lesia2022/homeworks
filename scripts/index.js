a06eb0ddaadbf3cb052baeb2c85fc739a2bac49d
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-13 16:17:59 +0200

    hm_13

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 'use strict';
+const age = prompt("Введите ваш возраст");
+if (isNaN(age)) {
+    alert("Вы ввели не число");
+} else {
+    let years = "";
+    const lastDigit = age % 10;
+    if (age >= 11 && age <= 14) {
+        years = "лет";
+    } else if (lastDigit === 1) {
+        years = "год";
+    } else if (lastDigit >= 2 && lastDigit <= 4) {
+        years = "года";
+    } else {
+        years = "лет";
+    }
+    alert(`Вам ${age} ${years}`);
+}
