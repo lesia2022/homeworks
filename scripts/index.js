a6a9459659e4ad5595c1998e0d11f5ef0d4b5cb5
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-16 11:44:29 +0200

    hm_12

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
 'use strict';
+const x = 4;
+const y = 9;
+const R = 10;
+const d = Math.sqrt(x*x + y*y);
+
+const message = (d <= R) ? 'The point lies inside a circle or on its boundary' : 'The point is outside the circle';
+console.log(message);
