20d276a33215247401b3283cead77f02434f1813
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-21 14:09:41 +0200

    hm_16

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const number = prompt('Enter the number :');
+const exponentiation= prompt('Enter the exponentiation:');
+
+function pow(number, exponentiation = 1) {
+    if (typeof number !== 'number' || typeof exponentiation !== 'number') {
+        return 'some error'
+    }
+    let result = Math.pow(number, exponentiation);
+    return result;
+}
+let result = pow(number, exponentiation);
+alert(result);
+
+
+
+
+
+
+
