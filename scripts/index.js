5ce8912018eba5e96833b81ea59f62153c06b747
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-21 18:15:09 +0200

    hm_22

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,39 @@
 'use strict';
+
+
+function findSmallestDivisor(number) {
+    if (number < 1 || !Number.isInteger(number)) {
+        return NaN;
+    }
+    for (let divisor = 2; divisor <= number; divisor++) {
+        if (number % divisor === 0) {
+            return divisor;
+        }
+    }
+}
+const userInput = prompt('Enter the number:');
+const number = parseInt(userInput);
+const smallestDivisor = findSmallestDivisor(number);
+console.log(`Нis the smallest divisor of a number ${number} equal ${smallestDivisor}`);
+
+
+
+
+
+
+//Through the while
+
+// const num = prompt('Enter the number:');
+// const number = Number(num);
+//
+// if (number < 1 || isNaN(number)) {
+//     console.log("NaN");
+// } else {
+//     let divisor = 2;
+//     while (number % divisor !== 0) {
+//         divisor++;
+//     }
+//     console.log(divisor);
+// }
+
+
