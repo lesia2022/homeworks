269d6cab10f620c89bff6beeab8d819a329e3e2c
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-21 17:34:25 +0200

    hm_19

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
 'use strict';
+function calculatePotatoesForBorsch(borschVolume) {
+    return Math.ceil(borschVolume * 4 * 75 / 1000) * 13;
+}
+const borschVolume = 48;
+console.log(`Total price of potatoes for ${borschVolume} liters of borsch is ${calculatePotatoesForBorsch(borschVolume)} UAH.`);
+
+
