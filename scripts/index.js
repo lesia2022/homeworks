66132216fcd47f3d89e0fc807712111c56b01308
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-22 14:29:04 +0200

    hm_18

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 'use strict';
+function isPositiveNumber(num) {
+    return typeof num === "number" && num > 0;
+}
+
+function getPositiveNumbers(arr) {
+    const exampleArr = [];
+
+    if (arr.length === 0) {
+        return "blank array";
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (isPositiveNumber(arr[i])) {
+            exampleArr.push(arr[i]);
+        }
+    }
+
+    if (exampleArr.length !== 0) {
+        return exampleArr;
+    } else {
+        return null;
+    }
+}
+
+const arr = [1, 2, 3, -1, -2, -3];
+console.log(getPositiveNumbers(arr)); // [1, 2, 3]
