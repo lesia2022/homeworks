cc0d44954657c517ab2e9da1fc2f5f8047b624d3
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-30 15:35:30 +0300

    hw 20

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,26 @@
 'use strict';
+const fib = (n = 1) => {
+    if (n === 0 || n === 1) {
+        return n;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+
+
+
+
+
+
+// console.log (fib());
+// console.log (fib(0));
+// console.log (fib (1));
+// console.log (fib (2));
+// console.log (fib (3));
+// console.log (fib (4));
+// console.log (fib (5));
+// console.log (fib (6));
+// console.log (fib (7));
+
+
+
