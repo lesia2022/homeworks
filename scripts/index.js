e2c8c1b8722083225d51a048360f942ae337bf95
lesia2022 <lesia.bezukh22@gmail.com>
2023-04-21 17:25:47 +0300

    hw 33

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 'use strict';
+function sum() {
+    let a = 0;
+    return function (b){
+        a += b;
+        return a;
+    }
+}
+const add = sum();
+
+console.log(add(3));
+console.log(add(5));
+console.log(add(20));
+console.log(add(30));
+
+
