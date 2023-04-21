bfe8ccfb36c7189ea93b6eca00a00e97543ef404
lesia2022 <lesia.bezukh22@gmail.com>
2023-04-21 18:26:19 +0300

    hw 31

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,11 @@ function palindromeCheck(number, steps = 0) {
     if (number.toString() === number.toString().split('').reverse().join('')) {
         return { result: number, steps: steps };
     }
-    else {
-        const newNumber = number + Number(number.toString().split('').reverse().join(''));
-        steps++;
-        return palindromeCheck(newNumber, steps);
-    }
+    const newNumber = number + Number(number.toString().split('').reverse().join(''));
+    steps++;
+    return palindromeCheck(newNumber, steps);
 }
+
 console.log(palindromeCheck(312));
 console.log(palindromeCheck(1234));
 console.log(palindromeCheck(96));
