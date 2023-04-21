33765cc64d40612ea5a5015714247be537ad49ec
lesia2022 <lesia.bezukh22@gmail.com>
2023-04-21 18:29:44 +0300

    hw 31

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,8 @@
 function getFactorial(n) {
     if (n <= 1) {
         return 1;
-    } else {
-        return n * getFactorial(n - 1);
     }
+        return n * getFactorial(n - 1);
 }
 console.log(getFactorial(3));
 
@@ -17,9 +16,8 @@ function pow(num, degree) {
         return num;
     } else if (degree < 0) {
         return 1 / pow(num, -degree);
-    } else {
-        return num * pow(num, degree - 1);
     }
+        return num * pow(num, degree - 1);
 }
 console.log(pow(2,6));
 
@@ -27,9 +25,8 @@ console.log(pow(2,6));
 function recursiveSum(a, b) {
     if (b === 0) {
         return a;
-    } else {
-        return recursiveSum(a + 1, b - 1);
     }
+        return recursiveSum(a + 1, b - 1);
 }
 console.log(recursiveSum(2,6));
 
