43cef58cbd92a1bca324274d80abc8ae4d9bb9d3
lesia2022 <lesia.bezukh22@gmail.com>
2023-04-25 22:25:26 +0300

    hw 34

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
 'use strict';
+function generateRandomNum(range) {
+    const numInRange = range => Math.floor(Math.random() * range) + 1;
+
+    const getNextNum = () => {
+        const numbers = [];
+        return () => {
+            let num = numInRange(range);
+            while (numbers.includes(num)) {
+                num = numInRange(range);
+            }
+            numbers.push(num);
+            console.log("number = " + num + " count = " + numbers.length);
+        };
+    };
+    const getNextUniqueNum = getNextNum();
+
+    for (let i = 0; i < range; i++) {
+        getNextUniqueNum();
+    }
+}
+
+const range = 100;
+
+generateRandomNum(range);
