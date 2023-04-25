5cdf31fb9cfacaab0bc2ec67b19096dc47308182
lesia2022 <lesia.bezukh22@gmail.com>
2023-04-25 11:55:23 +0300

    hw 32

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
 'use strict';
+const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+const customFlat = (arr) => {
+    if (!Array.isArray(arr)) {
+        throw new Error('Function accepts only 1 argument, too much arguments provided');
+    }
+
+    let resultArr = [];
+
+    const flatten = (arr) => {
+        for (const item of arr) {
+            if (Array.isArray(item)) {
+                flatten(item);
+            } else {
+                resultArr = [...resultArr, item];
+            }
+        }
+    }
+
+    flatten(arr);
+
+    return resultArr;
+}
+
+console.log(customFlat(arr));
