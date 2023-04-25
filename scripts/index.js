5d10c6ca0d23789d7e3f40e4969836bdae19540c
lesia2022 <lesia.bezukh22@gmail.com>
2023-04-25 18:58:43 +0300

    hw 36

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 'use strict';
+let calculator = {
+    sum() {
+        return this.a + this.b;
+    },
+
+    mul() {
+        return this.a * this.b;
+    },
+
+    read() {
+        let a = +prompt('Enter the first number', '0');
+        let b = +prompt('Enter the second number', '0');
+        if (isNaN(a) || isNaN(b)) {
+            return false;
+        }
+
+        this.a = +a;
+        this.b = +b;
+        return true;
+    }
+};
+
+if (calculator.read()) {
+    alert( calculator.sum() );
+    alert( calculator.mul() );
+} else {
+    alert('Invalid input. Please enter a numbers.');
+}
