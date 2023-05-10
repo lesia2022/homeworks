86bb08d2a503cdcfee23a4e6c8bf52ba281323d4
lesia2022 <lesia.bezukh22@gmail.com>
2023-05-10 12:43:41 +0300

    hw_42

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 'use strict';
+function generateList(array) {
+    const listItems = array.map((item) => {
+        if (Array.isArray(item)) {
+            const nestedList = generateList(item);
+            return `<li>${nestedList}</li>`;
+        } else {
+            return `<li>${item}</li>`;
+        }
+    });
+
+    const list = listItems.join('');
+    return `<ul>${list}</ul>`;
+}
+const array = [1, 2, [1.1, 1.2, 1.3], 3];
+const result = generateList(array);
+console.log(result);
