810e750019549af1df48efca190084d52f18dc70
lesia2022 <lesia.bezukh22@gmail.com>
2023-05-12 18:01:38 +0300

    hw_43

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
 'use strict';
+function createTable() {
+    const tableContainer = document.getElementById('myTable');
+
+    const table = document.createElement('table');
+
+    let count = 1;
+    for (let i = 0; i < 10; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 10; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = count++;
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+
+    tableContainer.appendChild(table);
+}
+
+createTable();
