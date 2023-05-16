cf75a3a5b9e10cb045f1b8c7d3c9170e6d358ea7
lesia2022 <lesia.bezukh22@gmail.com>
2023-05-16 17:58:18 +0300

    hw_44

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
 'use strict';
+const form = document.getElementById('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const email = formData.get('email');
+    const firstName = formData.get('firstName');
+    const lastName = formData.get('lastName');
+    const nickname = formData.get('nickname');
+    const password = formData.get('password');
+    const option = formData.get('option');
+    const message = formData.get('message');
+    const file = formData.get('file');
+    const terms = formData.get('terms');
+
+    const data = {
+        email,
+        firstName,
+        lastName,
+        nickname,
+        password,
+        option,
+        message,
+        file,
+        terms
+    };
+    console.log(data);
+});
