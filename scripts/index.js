6cb671b168a93543379aad34a36f17ab74301b72
lesia2022 <lesia.bezukh22@gmail.com>
2023-06-05 17:30:24 +0300

    hw_49

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,23 @@ void function () {
     const todoItemsContainer = document.getElementById('todoItems');
     const formInputs = Array.from(form.querySelectorAll('textarea, input:not([type=reset])'));
     const dataKey = 'todoListData';
+    const attributeKey = 'data-id';
+    const maxId = 'maxId';
 
-    const makeTodoItemTemplate = ({title, description}) => {
+    const makeTodoItemTemplate = ({ title, description, id }) => {
         const wrapper = document.createElement('div');
         wrapper.classList.add('col-4');
-
+        wrapper.setAttribute(attributeKey, id);
         wrapper.innerHTML = `
-            <div class="taskWrapper">
-                <div class="taskHeading">${title}</div>
-                <div class="taskDescription">${description}</div>
-            </div>`;
+        <div class="taskWrapper">
+            <div class="taskHeading">${title}</div>
+            <div class="taskDescription">${description}</div>
+            <div class="buttonWrapper">
+                <button class="mt-3 removeTask btn-danger btn-sm">
+                    <i class="bi bi-trash3"></i><span>Remove</span>
+                </button>
+            </div>
+        </div>`;
         return wrapper;
     };
 
@@ -24,20 +31,54 @@ void function () {
             throw new Error('Invalid DOM element');
         }
     };
+    const removeItemDOM = (item) => {
+        const element = document.querySelector(`[${attributeKey}='${item.id}']`);
 
-    const getTodoItem = () => {
-        let localStorageArray = JSON.parse(localStorage.getItem(dataKey));
-        if (!localStorageArray) localStorageArray = [];
-        return localStorageArray;
+        if (element) {
+            element.remove();
+        }
     };
 
+    const getAllItems = () => JSON.parse(localStorage.getItem(dataKey)) || [];
 
     const saveTodoItem = (formData) => {
-        const existData = getTodoItem();
-        existData.push(formData);
-        localStorage.setItem(dataKey, JSON.stringify(existData));
-        return getTodoItem().at(-1);
+        const existData = getAllItems();
+        const newId = localStorage.getItem(maxId) || 0;
+        const saveDataDuplicate = {
+            ...formData,
+            id: parseInt(newId) + 1
+        };
+
+        existData.push(saveDataDuplicate);
+
+        updateStorageWithItemAndMaxId(existData, saveDataDuplicate);
+
+        return saveDataDuplicate;
     };
+
+    function updateStorageWithItemAndMaxId(itemsToUpdate, saveDataDuplicate) {
+        localStorage.setItem(dataKey, JSON.stringify(itemsToUpdate));
+        localStorage.setItem(maxId, saveDataDuplicate.id);
+    }
+
+    const removeFromStorageById = (id) => {
+        const existData = getAllItems();
+        const indexTodoItem = existData.findIndex(item => item.id === id);
+
+        if (indexTodoItem === -1) {
+            return null;
+        }
+
+        const removedItem = existData.splice(indexTodoItem, 1);
+        localStorage.setItem(dataKey, JSON.stringify(existData));
+
+        return removedItem[0];
+    };
+
+    const removeElementById = (id) => {
+        const deletedElement = removeFromStorageById(id);
+        removeItemDOM(deletedElement)
+    }
 
     form.addEventListener('submit', event => {
         event.preventDefault();
@@ -47,24 +88,36 @@ void function () {
             acc[name] = value;
             return acc;
         }, {});
-        const savedElements = (saveTodoItem(formData))
-        saveTodoItem(formData);
+        const savedElement = saveTodoItem(formData);
 
         drawTodoItem(
-            makeTodoItemTemplate(savedElements)
+            makeTodoItemTemplate(savedElement)
         );
     });
 
+    todoItemsContainer.addEventListener('click', (event) => {
+        event.stopPropagation();
+        const removeTaskButton = event.target.closest('.removeTask');
+
+        if (!removeTaskButton) return;
+
+        const getTodoItemID = Number(event.target.closest(`[${attributeKey}]`).getAttribute(attributeKey));
+
+        removeElementById(getTodoItemID);
+    });
+
     const uploadHandler = () => {
-        const data = getTodoItem();
-        if(!data.length) return;
+        const data = getAllItems();
+        if (!data.length) return;
 
-        data.forEach(todoItem => {
-            const template = makeTodoItemTemplate(todoItem);
-            drawTodoItem(template)
-        })
-        document.removeEventListener('DOMContentLoaded', uploadHandler)
-    }
-    document.addEventListener('DOMContentLoaded', uploadHandler)
+        for (const { title, description, id } of data) {
+            const template = makeTodoItemTemplate({ title, description, id });
+            drawTodoItem(template);
+        }
+    };
 
+    document.addEventListener('DOMContentLoaded', uploadHandler);
+
+
+    document.addEventListener('DOMContentLoaded', uploadHandler);
 }();
