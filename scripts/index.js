329dcaa3a22b404facdc654365222a42597e30a6
lesia2022 <lesia.bezukh22@gmail.com>
2023-06-24 16:39:42 +0300

    hw_54

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,44 @@
 'use strict';
+'use strict';
+
+function DeepFreeze(obj) {
+    Object.freeze(obj);
+
+    Object.getOwnPropertyNames(obj).forEach((prop) => {
+        let propValue = obj[prop];
+        if (typeof propValue === "object" && propValue !== null && !Object.isFrozen(propValue)) {
+            obj[prop] = DeepFreeze(propValue);
+        }
+    });
+
+    return obj;
+}
+
+let user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            },
+        },
+    },
+};
+
+DeepFreeze(user);
+
+console.log(Object.isFrozen(user));
+console.log(Object.isFrozen(user.data));
+console.log(Object.isFrozen(user.data.d));
+console.log(Object.isFrozen(user.data.d.d1));
+
+user.data.a = 2;
+user.data.d.a1 = 110;
+user.data.d.d1.a2 = 1000;
