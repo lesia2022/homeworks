d430b180bd4a15487a24b3202e501e4f1fdb83c8
lesia2022 <lesia.bezukh22@gmail.com>
2023-06-29 16:41:21 +0300

    hw_69

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,25 @@
 'use strict';
 
 const gradeMap = new WeakMap();
+
 function saveGrades(student, grades) {
+    if (!isValidStudent(student)) {
+        throw new Error('Студент должен быть объектом.');
+    }
+
+    if (!isValidGrades(grades)) {
+        throw new Error('Оценки должны быть представлены объектом.');
+    }
+
     gradeMap.set(student, grades);
+}
+
+function isValidStudent(student) {
+    return student && typeof student === 'object' && !Array.isArray(student) && Object.keys(student).length !== 0;
+}
+
+function isValidGrades(grades) {
+    return grades && typeof grades === 'object' && !Array.isArray(grades) && Object.keys(grades).length !== 0;
 }
 
 const student = { name: 'Ben Affleck' };
