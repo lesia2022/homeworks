8994a10698e444ceb50d993e92b1e01ec7a27d2f
lesia2022 <lesia.bezukh22@gmail.com>
2023-06-29 16:06:31 +0300

    hw_67

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,24 @@
 'use strict';
+function totalValue (products) {
+    if (products.length === 0) {
+        return 0;
+    }
+
+    const productsPrices = new Map();
+
+    products.forEach(([item, price]) => {
+        productsPrices.set(item, price);
+    });
+
+    let totalCost = 0;
+    productsPrices.forEach(price => {
+        totalCost += price;
+    });
+
+    return totalCost;
+}
+
+
+const goods = [['t-shirt', 15], ['shorts', 25], ['socks', 5]];
+
+console.log(totalValue (goods));
