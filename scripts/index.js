c10862fd8dbeb95521ffb1b93d6ce67e93487218
lesia2022 <lesia.bezukh22@gmail.com>
2023-06-29 13:35:21 +0300

    hw_68

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
 'use strict';
+function uniqueCharacterCount(str) {
+    const uniqueCharter = new Set();
+    Array.from(str).forEach(symbol => {
+        uniqueCharter.add(symbol);
+    });
+    return uniqueCharter.size;
+}
+
+const string = "abracadabra";
+console.log(uniqueCharacterCount(string));
+
+
