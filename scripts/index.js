7695c28bc01e6380b2c56f9730b7e8da8e757e95
lesia2022 <lesia.bezukh22@gmail.com>
2023-06-29 16:34:26 +0300

    hw_69

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const gradeMap = new WeakMap();
+function saveGrades(student, grades) {
+    gradeMap.set(student, grades);
+}
+
+const student = { name: 'Ben Affleck' };
+const grades = { math: 90, fiz: 85, eng: 95 };
+
+saveGrades(student, grades);
+console.log(gradeMap.get(student));
