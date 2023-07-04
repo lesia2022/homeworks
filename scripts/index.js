ba89206d2c31ef6950af340d80c03cec93a3eba8
lesia2022 <lesia.bezukh22@gmail.com>
2023-07-04 16:17:07 +0300

    hw_71

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 'use strict';
+function printGreetings(students) {
+    const greetings = [];
+
+    if (students.length === 0) {
+        throw new Error("No students found.");
+    }
+
+    for (const { name, subject } of students) {
+        greetings.push(`Hello, ${name}! You study ${subject}!`);
+    }
+
+    return greetings;
+}
+
+const students = [
+    { name: 'John', age: 20, subject: 'Math' },
+    { name: 'Alice', age: 22, subject: 'English' },
+    { name: 'Bob', age: 21, subject: 'History' }
+];
+
+const greetings = printGreetings(students);
+
+for (const greeting of greetings) {
+    console.log(greeting);
+}
+
